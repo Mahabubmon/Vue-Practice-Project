dc3d5a20693c3493e8692ad2f540339ffc0a396c
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-13 11:35:32 +0600

    update_page-added

---
+++ src/components/UpdateVue.vue
@@ -0,0 +1,14 @@
+<template>
+    <HeaderVue /> 
+    
+       
+    </template>
+    
+    <script>
+    import HeaderVue from './HeaderVue.vue';
+    import axios from "axios"
+    export default{
+        name:"UpdateVue",
+        
+    }
+    </script>
