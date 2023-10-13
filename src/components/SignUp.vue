c49c66804e6221fc5c0a6e9ebb9a27a8d48d1501
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-13 11:34:13 +0600

    signup_registry_button_funtionality

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -18,6 +18,43 @@
 import axios from 'axios'
 export default {
     name : 'SignUp',
+    data(){
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async signUp()
+        {
+            console.warn("signUp",this.name,this.email,this.password)
+            let result = await axios.post("http://localhost:3000/users",
+            {
+                email:this.email,
+                name:this.name,
+                password:this.password
+
+            });
+            console.warn(result);
+            if(result.status==201)
+            {   
+                localStorage.setItem("user-info",JSON.stringify(result.data))
+                this.$router.push({name:'HomeVue'});
+
+            }
+            
+        }
+
+    },
+    mounted()
+    {
+        let user = localStorage.getItem('user-info');
+        if(user)
+        {
+            this.$router.push({name:'HomeVue'});
+        }
+    }
     
 }
 </script>
