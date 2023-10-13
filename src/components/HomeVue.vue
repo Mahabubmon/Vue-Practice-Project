201ee3da9927654786f1cb5b4e2c7b0dc8a1e0e6
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-13 11:24:34 +0600

    Functionality

--- src/components/HomeVue.vue
+++ src/components/HomeVue.vue
@@ -1,5 +1,23 @@
 <template>
     <HeaderVue /> 
+    <table border="1">
+    <tr>
+    <td>ID</td>
+    <td>NAME</td>
+    <td>ADDRESS</td>
+    <td>CONTECT</td>
+    <td>Action</td>
+
+    </tr>
+    <tr v-for="item in resturent" :key="item.id">
+    <td>{{ item.id }}</td>
+    <td>{{ item.name }}</td>
+    <td>{{ item.address }}</td>
+    <td>{{ item.contect }}</td>
+    <td><router-link :to="'/update/'+item.id">Update</router-link></td>
+    <button v-on:click="deleteResturent(item.id)">Delete</button>
+    </tr>
+</table>
 
        
     </template>
@@ -8,7 +26,41 @@
     import axios from 'axios';
     import HeaderVue from './HeaderVue.vue';
     export default{
-        name:"HomeVue",
+        name:"HomeVue", 
+        data()
+    {
+        return {
+            name:'',
+            resturent:[],
+        }
+    },
+    components:{
+        HeaderVue
+    },
+    methods:{
+        async deleteResturent(id){
+            let result = await axios.delete("http://localhost:3000/resturent/"+id);
+            if(result.status==200){
+                this.loadData();
+
+            }
+        },
+        async loadData(){
+            let user = localStorage.getItem('user-info');
+        this.name= JSON.parse(user).name
+        if(!user)
+        {
+            this.$router.push({name:'SignUp'});
+        }
+        let result = await axios.get("http://localhost:3000/resturent");
+        console.warn(result)
+        this.resturent = result.data;
+        }
+    },
+    mounted()
+    {
+        this.loadData();
+    }
         
     }
     </script>
