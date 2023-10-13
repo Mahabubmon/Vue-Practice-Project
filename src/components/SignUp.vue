2b9c2bd3c21795f5bd7a1f843b706d7fa010f92c
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-13 11:33:17 +0600

    signup_registry_button-added

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,4 +1,15 @@
 <template>
+    <img class="logo" src="../assets/restologo.png" >
+    <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" v-model="name" placeholder="Enter Name"/>
+        <input type="text" v-model="email" placeholder="Enter Email"/>
+        <input type="password" v-model="password" placeholder="Enter Password"/>
+        <button v-on:click="signUp">Sign Up</button>
+        <p>
+            <router-link to="/login">Login</router-link>
+        </p>
+    </div>
     
 </template>
 
