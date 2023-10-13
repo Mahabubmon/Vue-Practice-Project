12f4561577290453249681ea77849d8520b81fca
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-13 11:18:58 +0600

    Addvue_file Added

---
+++ src/components/AddVue.vue
@@ -0,0 +1,16 @@
+<template>
+    <HeaderVue /> 
+    <h1>Add Resturent </h1>
+       
+    </template>
+    
+    <script>
+    import HeaderVue from './HeaderVue.vue';
+    import axios from 'axios'
+    export default{
+        name:"AddVue",
+        components:{
+            HeaderVue
+        },
+         }
+    </script>
