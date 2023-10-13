f91585474c8de10d24682c0f1db37436f2caf441
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-13 11:19:41 +0600

    Addvue_file_form_ Added

--- src/components/AddVue.vue
+++ src/components/AddVue.vue
@@ -1,6 +1,12 @@
 <template>
     <HeaderVue /> 
     <h1>Add Resturent </h1>
+    <form class="add">
+        <input type="text" placeholder="Enter Name" v-model="resturent.name" name="name"/>
+        <input type="text" placeholder="Enter Address" v-model="resturent.address" name="address"/>
+        <input type="text" placeholder="Enter contect" v-model="resturent.contect" name="contect"/>
+        <button type="button" v-on:click="addresturent">Add New Resturent</button>
+    </form>
        
     </template>
     
