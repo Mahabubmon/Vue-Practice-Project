493ccf9432b1d1abd3ba7f24d578cf649f5bc264
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-13 11:29:52 +0600

    login_page_button-modification

--- src/components/LoginVue.vue
+++ src/components/LoginVue.vue
@@ -1,4 +1,15 @@
 <template>
+    <img class="logo" src="../assets/restologo.png" >
+<h1>Login Page</h1>
+<div class="login">
+        
+        <input type="text" v-model="email" placeholder="Enter Email"/>
+        <input type="password" v-model="password" placeholder="Enter Password"/>
+        <button v-on:click="login" >Login</button>
+        <p>
+            <router-link to="/sign-up">SignUp</router-link>
+        </p>
+    </div>    
     
     </template>
     
