44dfd0fc86b37bd11da5c8c6e274ed9ca539539f
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-22 19:39:38 +0600

    ad_to_home-component

---
+++ src/components/AddMenu.vue
@@ -0,0 +1,15 @@
+<template>
+        <h1>Add Resturent </h1>
+    <form class="add">
+        <input type="text" placeholder="Enter Name" v-model="resturent.name" name="name"/>
+        <input type="text" placeholder="Enter Address" v-model="resturent.address" name="address"/>
+        <input type="text" placeholder="Enter contect" v-model="resturent.contect" name="contect"/>
+        <button type="button" v-on:click="addresturent">Add New Resturent</button>
+    </form>
+</template>
+
+<script>
+export default{
+    name: "AddMenu"
+}
+</script>
