1274b31b7a8ce72d6eb770f77be5b4e7e034c56a
Mahabubur Rahman <mahabubmon@gmail.com>
2024-02-06 22:11:27 +0600

    student page

--- vue-project/src/views/Students/View.vue
+++ vue-project/src/views/Students/View.vue
@@ -1,5 +1,51 @@
 <template>
-    <div class="about">
-        <h1>This is  Student page</h1>
+   <div class="card">
+    <div class="card-header">
+        <h4>
+            Students
+            <RouterLink to="/student/create" class="btn btn-primary float-end" >
+                Add Student
+            </RouterLink>
+        </h4>
     </div>
+    <div class="card-body">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Created At</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+   </div>
 </template>
+
+
+<script>
+export default {
+    name: 'student',
+    data() {
+        return {
+            student:[]
+            
+        }
+    },
+    mounted() {
+        console.log('I am here');
+    }
+}
+
+
+</script>
