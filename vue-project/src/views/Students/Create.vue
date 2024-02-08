619f28c1161ed2dbacf9e13801ab755ca6819312
Mahabubur Rahman <mahabubmon@gmail.com>
2024-02-08 23:19:42 +0600

    data sending through the api

--- vue-project/src/views/Students/Create.vue
+++ vue-project/src/views/Students/Create.vue
@@ -7,27 +7,63 @@
             <div class="card-body">
                 <div class="mb-3">
                     <label for="">Name</label>
-                    <input type="text" class="form-control"/>
+                    <input type="text" v-model="model.student.name" class="form-control"/>
                 </div>
                 <div class="mb-3">
                     <label for="">Course</label>
-                    <input type="text" class="form-control"/>
+                    <input type="text" v-model="model.student.course" class="form-control"/>
                 </div>
                 <div class="mb-3">
                     <label for="">Email</label>
-                    <input type="text" class="form-control"/>
+                    <input type="text" v-model="model.student.email" class="form-control"/>
                 </div>
                 <div class="mb-3">
                     <label for="">Phone</label>
-                    <input type="text" class="form-control"/>
+                    <input type="text" v-model="model.student.phone" class="form-control"/>
                 </div>
                 <div class="mb-3">
-                    <button type="button" class="btn btn-primary">Save</button>
+                    <button type="button" @click="saveStudent" class="btn btn-primary">Save</button>
                 </div>
             </div>
         </div>
     </div>
-
-
-
 </template>
+
+<script>
+export default {
+    name: 'studentCreate',
+    data() {
+        return {
+            model: {
+                student: {
+                    name: '',
+                    course:'',
+                    email:'',
+                    phone:''
+                }
+            }
+        }
+    },
+    methods: {
+        saveStudent() {
+            axios.post('http://localhost:8000/api/students', this.model.student)
+                .then(rest => {
+                    console.log(res.data)
+                    alert(res.data.message);
+                    this.student = {
+                        name: '',
+                        course: '',
+                        email: '',
+                        phone: ''
+                    }
+                
+                })
+                .catch(function (error) {
+                if()
+            })
+            
+        }
+    }
+}
+
+</script>
